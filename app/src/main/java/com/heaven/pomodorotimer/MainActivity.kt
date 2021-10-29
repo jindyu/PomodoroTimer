923f06b561fd73afd55812a8d022c5264ba79c93
jin <program@hanbitsoft.co.kr>
2021-10-29 21:04:43 +0900

    first commit

---
+++ app/src/main/java/com/heaven/pomodorotimer/MainActivity.kt
@@ -0,0 +1,137 @@
+package com.heaven.pomodorotimer
+
+import android.annotation.SuppressLint
+import android.media.SoundPool
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var tickingSoundId: Int? = null
+    private var bellSoundId: Int? = null
+
+    private val remainMinutesTextView: TextView by lazy {
+        findViewById(R.id.remainMinutesTextView)
+    }
+
+    private val remainSecondsTextView: TextView by lazy {
+        findViewById(R.id.remainSecondesTextView)
+    }
+
+    private val seekBar: SeekBar by lazy{
+        findViewById(R.id.seekBar)
+    }
+
+    private val soundPool = SoundPool.Builder().build()
+
+    private var currentCountDownTimer: CountDownTimer? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bindView()
+        initSounds()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        soundPool.autoResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        soundPool.autoPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release()
+    }
+
+    private fun initSounds() {
+        tickingSoundId = soundPool.load(this, R.raw.timer_ticking, 1)
+        bellSoundId = soundPool.load(this, R.raw.timer_bell, 1)
+    }
+
+    private fun bindView() {
+        seekBar.setOnSeekBarChangeListener(
+            object: SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                    if(fromUser){
+                        updateRemainTime(progress * 60 * 1000L)
+                    }
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    stopCountDown()
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    seekBar ?: return
+
+                    if(seekBar.progress == 0){
+                        stopCountDown()
+                    } else {
+                        startCountDown()
+                    }
+                }
+            }
+        )
+    }
+
+    private fun stopCountDown(){
+        currentCountDownTimer?.cancel()
+        currentCountDownTimer = null
+        soundPool.autoPause()
+    }
+
+    private fun startCountDown(){
+        currentCountDownTimer = createCountDownTimer(seekBar.progress * 80 * 1000L)
+        currentCountDownTimer?.start()
+
+        tickingSoundId?.let{  soundId ->
+            soundPool.play(soundId, 1f, 1f, 0, -1, 1f)
+        }
+    }
+
+    private fun createCountDownTimer(initialMillis: Long) : CountDownTimer{
+        return object: CountDownTimer(initialMillis, 1000L){
+            override fun onTick(millisUntilFinished: Long) {
+                updateRemainTime(millisUntilFinished)
+                updateSeekBar(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                completeCountDown()
+            }
+        }
+    }
+
+    private fun completeCountDown(){
+        updateRemainTime(0)
+        updateSeekBar(0)
+
+        soundPool.autoPause()
+        bellSoundId?.let{soundId ->
+            soundPool.play(soundId, 1f, 1f, 0, 0, 1f)
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateRemainTime(remainMillis: Long) {
+        val remainSeconds = remainMillis / 1000
+        remainMinutesTextView.text = "%02d'".format(remainSeconds / 60)
+        remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
+    }
+
+    private fun updateSeekBar(remainMillis: Long){
+        seekBar.progress = (remainMillis / 1000 / 60).toInt()
+    }
+}
